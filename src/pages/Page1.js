ddcb0bb08bbdb55f283ceaee6f96f1ed8573cc90
Jeehay Park <jhpark@ictk.com>
2024-06-12 15:13:06 +0900

    Add a picture

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CenterBox from "./CenterBox";
+import pic from "../assets/Theo-van-Doesburg.jpg"
 
 export default function Page1() {
   return (
     <>
       <CenterBox>
             <h1 style={{color : 'green', fontWeight : 'bold'}}>This page is for testing the deployment of this web application.</h1>
+            <img src={pic} alt="Theo vanDoesburg"/>
       </CenterBox>
     </>
   );
